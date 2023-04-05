7695e7029b69c458184a86d0de62fb3597c07adf
guille <guillenazua@gmail.com>
2023-04-05 12:13:58 +0100

    Adding "ProductContext.cs" to allowing the access to data

---
+++ Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using comfortWalk.Models;
+using System.Data.Entity;
+namespace comfortWalk
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
