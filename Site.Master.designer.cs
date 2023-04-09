f89e868d0155ab72d5e19d6b210fea067605e4a0
guille <guillenazua@gmail.com>
2023-04-09 21:05:34 +0100

    Adding a Shopping Cart Counter

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace comfortWalk
     {
 
         /// <summary>
+        /// cartCount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor cartCount;
+
+        /// <summary>
         /// categoryList control.
         /// </summary>
         /// <remarks>
