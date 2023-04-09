0595d5bb5b6552803bab1203cccaae4f2d3f8fdb
guille <guillenazua@gmail.com>
2023-04-09 21:07:09 +0100

    adding new feature to site master. This time is for allowing the user to see the number of items in the shopping cart

--- Site.Master.cs
+++ Site.Master.cs
@@ -9,6 +9,7 @@ using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
 using System.Linq;
 using comfortWalk.Models;
+using comfortWalk.Logic;
 
 namespace comfortWalk
 {
@@ -72,6 +73,14 @@ namespace comfortWalk
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                string cartStr = string.Format("Cart ({0})", usersShoppingCart.GetCount());
+                cartCount.InnerText = cartStr;
+            }
         }
         public IQueryable<Category> GetCategories()
         {
