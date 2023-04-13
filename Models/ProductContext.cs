c2c7584612bfeed41fb848e68b9a35e17dcea617
guille <guillenazua@gmail.com>
2023-04-13 16:38:45 +0100

    Adding new functionalities. This time, for checkout payment and security funcionalities

--- Models/ProductContext.cs
+++ Models/ProductContext.cs
@@ -13,6 +13,8 @@ namespace comfortWalk.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<CartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
     }
 }
